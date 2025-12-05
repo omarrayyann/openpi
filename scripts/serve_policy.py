916b3f28c5a03909cfa0a5a9e79dbe499569fba0
Omar Rayyan orayyan@cs.ucla.edu <orayyan@uril-2>
2025-12-04 23:18:37 -0800

    added policy name to server metadata

--- scripts/serve_policy.py
+++ scripts/serve_policy.py
@@ -1,3 +1,4 @@
+import os
 import dataclasses
 import enum
 import logging
@@ -107,7 +108,7 @@ def create_policy(args: Args) -> _policy.Policy:
 def main(args: Args) -> None:
     policy = create_policy(args)
     policy_metadata = policy.metadata
-
+    policy_metadata.policy_name = os.path.basename(args.policy.dir)
     # Record the policy's behavior.
     if args.record:
         policy = _policy.PolicyRecorder(policy, "policy_records")
